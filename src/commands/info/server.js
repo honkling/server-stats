bd529805b22155eba60b45e71542cb5f09de54dc
Goose Go Honk <untitledgeese@gmail.com>
2021-03-03 01:27:46 +0000

    Add more properties to server command

--- src/commands/info/server.js
+++ src/commands/info/server.js
@@ -26,13 +26,34 @@ module.exports = class ServerCommand extends Command {
 		const res = await get(url);
 		if(res.status !== 200) return msg.reply('couldn\'t find a server with that name or ID!');
 		const info = res.body.server;
+		const all_plugins_res = await get('https://api.minehut.com/plugins_public');
+		const all_plugins = all_plugins_res.body.all;
+		let plugins = [];
+		for(const i of info.active_plugins) {
+			plugins.push(all_plugins.filter(x => x._id === i)[0].name);
+		}
+		let properties = '';
+		for(const i in info.server_properties) {
+			const value = info.server_properties[i];
+			properties += `**${(() => {
+				const arr = i.split('_');
+				for(const o of arr) {
+					arr[arr.indexOf(o)] = o[0].toUpperCase() + o.substr(1).toLowerCase()
+				}
+				const builder = arr.join(' ');
+				return builder;
+			})()}** \`${String(value) === '' ? ' ' : String(value).replace(/`/g, '').substr(0, 50)}\`\n`;
+		}
 		msg.channel.send(
 			new MessageEmbed()
-				.setTitle(info.name)
-				.setColor('BLUE')
-				.setDescription(info.motd)
+				.setTitle(`${info.name} ${info.visibility ? '' : '(unlisted)'}`)
+				.setColor(info.online ? 'GREEN' : 'RED')
+				.setDescription(info.motd.replace(/(&|§)([A-Fa-f0-9blkuo])/g, ''))
+				.addField('Player Count', `${info.playerCount}/${info.maxPlayers}`, true)
 				.addField('Server ID', info._id, true)
-				.addField('Online?', String(info.online), true)
+				.addField('Suspended?', String(info.suspended), true)
+				.addField('Plugins', plugins.map(p => `⋆ ${p}`).join('\n'))
+				.addField('Server Properties', properties.trim())
 				.setFooter(`Requested by ${msg.author.tag}`, msg.author.displayAvatarURL())
 		);
 	}
